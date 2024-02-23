bb4489fa8c7ee42c042490421b96bb0fbfb1a59c
ndry0611 <andryss0611@gmail.com>
2024-02-23 22:48:46 +0700

    add basic crud for sparePart endpoint

--- src/modules/SparePart/SparePartsHandler.js
+++ src/modules/SparePart/SparePartsHandler.js
@@ -1,5 +1,8 @@
 import {
-    getAllSparePartController
+    getAllSparePartController,
+    createSparePartController,
+    updateSparePartController,
+    deletSparePartController
 } from "./SparePartsController.js"
 
 async function sparePartRoute(fastify, options, next) {
@@ -50,18 +53,16 @@ async function sparePartRoute(fastify, options, next) {
                     name: { type: "string" },
                     part_no: { type: "string" },
                     genuine: { type: "boolean" },
-                    stock: { type: "integer", minimum: "0" },
+                    stock: { type: "integer", minimum: 0 },
                     capital_price: { type: "integer" },
                     sell_method: { type: "integer" },
                     is_available: { type: "boolean" },
                     sale_price: { type: "integer" },
                     description: { type: "string" },
                     supply_date: { type: "string", format: "date-time" },
-                    created_at: { type: "string", format: "date-time" },
-                    updated_at: { type: "string", format: "date-time" }
                 },
                 additionalProperties: false,
-                required: ['id_spare_part_brand', 'id_category', 'name', 'part_no', 'genuine', 'stock', 'capital_price', 'sell_method', 'is_available', 'sale_price', 'description', 'supply_date', 'created_at', 'updated_at']
+                required: ['id_spare_part_brand', 'id_category', 'name', 'part_no', 'genuine', 'stock', 'capital_price', 'sell_method', 'is_available', 'sale_price', 'description', 'supply_date']
             },
             response: {
                 201: {
@@ -75,7 +76,7 @@ async function sparePartRoute(fastify, options, next) {
                         name: { type: "string" },
                         part_no: { type: "string" },
                         genuine: { type: "boolean" },
-                        stock: { type: "integer", minimum: "0" },
+                        stock: { type: "integer", minimum: 0 },
                         capital_price: { type: "integer" },
                         sell_method: { type: "integer" },
                         is_available: { type: "boolean" },
@@ -91,6 +92,87 @@ async function sparePartRoute(fastify, options, next) {
         preHandler: [fastify.authenticate, fastify.isAdmin]
     }
     fastify.post('/', createSparePartSchema, createSparePartController);
+
+    const updateSparePartSchema = {
+        schema: {
+            params: {
+                type: "object",
+                properties: {
+                    id: { type: "integer" }
+                },
+                required: ['id']
+            },
+            body: {
+                type: "object",
+                properties: {
+                    id_spare_part_brand: { type: "integer" },
+                    id_category: { type: "integer" },
+                    id_car: { type: "integer" },
+                    id_supplier: { type: "integer" },
+                    name: { type: "string" },
+                    part_no: { type: "string" },
+                    genuine: { type: "boolean" },
+                    stock: { type: "integer", minimum: 0 },
+                    capital_price: { type: "integer" },
+                    sell_method: { type: "integer" },
+                    is_available: { type: "boolean" },
+                    sale_price: { type: "integer" },
+                    description: { type: "string" },
+                    supply_date: { type: "string", format: "date-time" },
+                },
+                additionalProperties: false,
+            },
+            responses: {
+                200: {
+                    type: "object",
+                    properties: {
+                        id: { type: "integer" },
+                        id_spare_part_brand: { type: "integer" },
+                        id_category: { type: "integer" },
+                        id_car: { type: "integer" },
+                        id_supplier: { type: "integer" },
+                        name: { type: "string" },
+                        part_no: { type: "string" },
+                        genuine: { type: "boolean" },
+                        stock: { type: "integer", minimum: 0 },
+                        capital_price: { type: "integer" },
+                        sell_method: { type: "integer" },
+                        is_available: { type: "boolean" },
+                        sale_price: { type: "integer" },
+                        description: { type: "string" },
+                        supply_date: { type: "string", format: "date-time" },
+                        updated_at: { type: "string", format: "date-time" },
+                    },
+                    required: ['id', 'id_spare_part_brand', 'id_category', 'name', 'part_no', 'genuine', 'stock', 'capital_price', 'sell_method', 'is_available', 'sale_price', 'description', 'supply_date', 'updated_at']
+                }
+            }
+        },
+        preHandler: [fastify.authenticate, fastify.isAdmin]
+    }
+    fastify.put('/:id', updateSparePartSchema, updateSparePartController);
+
+    const deleteSparePartSchema = {
+        schema: {
+            params: {
+                type: "object",
+                properties: {
+                    id: { type: "integer" }
+                },
+                required: ['id']
+            },
+            response: {
+                200: {
+                    type: "object",
+                    properties: {
+                        message: { type: "string" }
+                    },
+                    required: ['message']
+                }
+            }
+        },
+        preHandler: [fastify.authenticate, fastify.isAdmin]
+    }
+    fastify.delete('/:id', deleteSparePartSchema, deletSparePartController);
 
     next()
 }
