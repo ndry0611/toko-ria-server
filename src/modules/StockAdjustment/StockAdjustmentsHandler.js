9fbbd356fb4768aaf503fae01be828281332d833
ndry0611 <andryss0611@gmail.com>
2024-05-09 00:54:01 +0700

    feat: add response data to stockAdjustment

--- src/modules/StockAdjustment/StockAdjustmentsHandler.js
+++ src/modules/StockAdjustment/StockAdjustmentsHandler.js
@@ -22,6 +22,7 @@ async function stockAdjustmentRoute(fastify, options, next) {
                         type: "object",
                         properties: {
                             id: { type: "integer" },
+                            code: { type: "string" },
                             id_spare_part: { type: "integer" },
                             SparePart: {
                                 type: "object",
@@ -42,7 +43,7 @@ async function stockAdjustmentRoute(fastify, options, next) {
                             created_at: { type: "string", format: "date-time" },
                             updated_at: { type: "string", format: "date-time" }
                         },
-                        required: ['id', 'id_spare_part', 'SparePart', 'old_quantity', 'new_quantity', 'created_at', 'updated_at']
+                        required: ['id', 'code', 'id_spare_part', 'SparePart', 'old_quantity', 'new_quantity', 'created_at', 'updated_at']
                     }
                 }
             }
