5b06a82d170df34619837646bc809e121de71ae1
ndry0611 <andryss0611@gmail.com>
2024-06-08 11:59:51 +0700

    fix: integration bug

--- src/modules/Midtrans/MidtransController.js
+++ src/modules/Midtrans/MidtransController.js
@@ -1,7 +1,31 @@
-import { handlingNotification } from "../../utils/snap.js";
+import { snap } from "../../utils/snap.js";
 
 export async function notificationHandlingController(request, reply) {
-  const body = request.body;
-  await handlingNotification(body);
-  return reply.code(200).send({message: "integration test"});
+  const notificationJson = request.body;
+  snap.transaction.notification(notificationJson)
+    .then((statusResponse) => {
+      let orderId = statusResponse.order_id
+      let transactionStatus = statusResponse.transaction_status;
+      let fraudStatus = statusResponse.fraud_status;
+      let summary = `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`;
+      if (transactionStatus == 'capture') {
+        if (fraudStatus == 'accept') {
+          // TODO set transaction status on your database to 'success'
+          // and response with 200 OK
+        }
+      } else if (transactionStatus == 'settlement') {
+        // TODO set transaction status on your database to 'success'
+        // and response with 200 OK
+      } else if (transactionStatus == 'cancel' ||
+        transactionStatus == 'deny' ||
+        transactionStatus == 'expire') {
+        // TODO set transaction status on your database to 'failure'
+        // and response with 200 OK
+      } else if (transactionStatus == 'pending') {
+        // TODO set transaction status on your database to 'pending' / waiting payment
+        // and response with 200 OK
+      }
+      console.log(summary)
+      reply.code(200).send({message: summary});
+    });
 }
--- src/utils/snap.js
+++ src/utils/snap.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import midtransClient from 'midtrans-client';
 
-const snap = new midtransClient.Snap({
+export const snap = new midtransClient.Snap({
   isProduction: false,
   serverKey: process.env.SERVER_KEYS_MT,
   clientKey: process.env.CLIENT_KEYS_MT
@@ -15,31 +15,4 @@ export async function createSnapTransaction(parameter) {
   } catch (error) {
     throw Error(error.message);
   }
-}
-
-export async function handlingNotification(notificationJson) {
-  await snap.transaction.notification(notificationJson)
-    .then((statusResponse) => {
-      let orderId = statusResponse.order_id
-      let transactionStatus = statusResponse.transaction_status;
-      let fraudStatus = statusResponse.fraud_status;
-      console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
-      if (transactionStatus == 'capture') {
-        if (fraudStatus == 'accept') {
-          // TODO set transaction status on your database to 'success'
-          // and response with 200 OK
-        }
-      } else if (transactionStatus == 'settlement') {
-        // TODO set transaction status on your database to 'success'
-        // and response with 200 OK
-      } else if (transactionStatus == 'cancel' ||
-        transactionStatus == 'deny' ||
-        transactionStatus == 'expire') {
-        // TODO set transaction status on your database to 'failure'
-        // and response with 200 OK
-      } else if (transactionStatus == 'pending') {
-        // TODO set transaction status on your database to 'pending' / waiting payment
-        // and response with 200 OK
-      }
-    });
 }
